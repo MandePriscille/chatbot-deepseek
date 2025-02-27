7424e230b5bdffe8ced6c749d11625dcae0346c0
MandePriscille <mandepriscille1@gmail.com>
2025-02-27 22:21:55 +0100

    fix: configuration project file

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
+DEEPSEEK_API_KEY = config('DEEPSEEK_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
